c9c813a3a1a56df38e1d2e10880591e849312308
Mateo Poblete <Mateopoblete28@gmail.com>
2026-06-05 19:05:33 -0300

    3er commit

---
+++ Python/2_miVariable.py
@@ -0,0 +1,13 @@
+miVariable = 3
+print(miVariable)
+miVariable = "Hola profe Ariel"
+print(miVariable)
+miVariable = 3.5
+print(miVariable)
+x = 10
+y = 2
+z = x + y
+print(id(x))
+
+print(id(y))
+print(id(z))
--- Python/3.1_Datos_de_Python.py
+++ Python/3.1_Datos_de_Python.py
@@ -1,19 +1,4 @@
 """
-miVariable = 3
-print(miVariable)
-miVariable = "Hola profe Ariel"
-print(miVariable)
-miVariable = 3.5
-print(miVariable)
-x = 10
-y = 2
-z = x + y
-print(id(x))
-
-print(id(y))
-print(id(z))
-"""
-"""
 x = 10
 print(x)
 print(type(x))
